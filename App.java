1f09398511d684aacf9de19d3f993b08a050aebc
sreeja434 <sirichowdary@gmail.com>
2025-07-22 12:40:30 +0530

    Frist_Fundamentals

---
+++ App.java
@@ -0,0 +1,10 @@
+class App{
+    // if you want to run a java program, class and main method 
+    // are must
+    // public - access modifier
+    // static - no object is required to run program
+    // void - retrun nothing
+    public static void main(String[] args) {
+        System.out.println("Hello World v2");
+    }
+}
---
+++ first_fundamentals/App.java
@@ -0,0 +1,13 @@
+package first_fundamentals;
+
+public class App {
+    // if you want to run a java program, class and main method
+    // are must
+    // public - access modifier
+    // static - no object is required to run program
+    // void - retrun nothing
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
